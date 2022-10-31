ce18331550a04467c4bd42923440b2ae5866f7f8
xooslo <ys2110205@y-y.hs.kr>
2022-10-31 11:12:30 +0900

    update

--- fash_front/js/script.js
+++ fash_front/js/script.js
@@ -1,8 +1,8 @@
 $(".mainMenu").hover(
-    function() {
+    function () {
         $(this).children(".subMenu").stop().slideDown("slow");
     },
-    function() {
+    function () {
         $(this).children(".subMenu").stop().slideUp("fast");
     }
 );
@@ -14,24 +14,24 @@ var timer = setInterval("autoslide()", 8000); // 3초마다 모션함수 실행
 
 // 모션함수
 function autoslide() {
-    
+
     // 첫번째 이미지 사라지기
     $(slide[sno]).stop().animate(
         {
             left: "100%"
         },
         1000,
-        function() {
+        function () {
             $(this).css({
-            
-             left: "-100%"
+
+                left: "-100%"
             });
         }
     );
 
     // 나타날 이미지 카운트
     sno++;
-    if(sno > eno) {
+    if (sno > eno) {
         sno = 0;
     }
 
@@ -41,6 +41,7 @@ function autoslide() {
     }, 1000);
 };
 
+/*
 var logo = $(".logo_div > img");
 var lno = 0;
 var ino = logo.length - 1;
@@ -71,4 +72,25 @@ function autoSlide() {
     $(logo[lno]).stop().animate({
         left: "0"
     }, 1000);
-};
+}; */
+
+var logo = $(".logo_div > img");
+var lno = 0;
+var ino = logo.length - 1;
+var imgArray = new Array();
+
+imgArray[0] = "./images/logo_black.png";
+imgArray[1] = "./images/logo_green.png";
+imgArray[2] = "./images/logo_orange.png";
+
+function showImage() {
+    var objImg = document.getElementById("logo");
+
+    objImg.src = imgArray[lno];
+    setTimeout(showImage, 8000);
+
+    lno++;
+    if(lno > ino) {
+        lno = 0;
+    }
+}
